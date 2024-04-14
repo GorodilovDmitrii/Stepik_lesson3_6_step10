6389fd6ac04187f40a62de73edd011200a4166a4
GorodilovDmitrii <151391441+GorodilovDmitrii@users.noreply.github.com>
2024-04-14 19:29:51 +0300

    Update test_items.py because of incorrect name of function

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-def test_stepik(browser):
+def test_item_has_add_to_cart_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     browser.implicitly_wait(5)
     add_to_basket = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
